7fe6317c16f768dcc605ed4e1e790cd1c06daf7f
Ariel Ma <ariel.ma822@gmail.com>
2017-12-23 14:56:57 +1100

    4.2 quiz to get airport list

---
+++ 4.2_airport_list/airport_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports()' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+
+Refer to the 'options.html' file in the tab above for a stripped down version
+of what is actually on the website. The test() assertions are based on the
+given file.
+"""
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        airports = soup.find('select', {'id': 'AirportList'}).find_all('option')
+        data = [airport.get('value') for airport in airports if not airport.get('value').startswith('All')]
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+if __name__ == "__main__":
+    test()
