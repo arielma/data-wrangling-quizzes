ff027349b1903e7b8eaaa507221e5bb65187045f
Ariel Ma <ariel.ma822@gmail.com>
2017-12-23 16:54:44 +1100

    4.6 quiz to split xml into smaller files due to multiple root rags

---
+++ 4.6_processing_patents/split_data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# So, the problem is that the gigantic file is actually not a valid XML, because
+# it has several root elements, and XML declarations.
+# It is, a matter of fact, a collection of a lot of concatenated XML documents.
+# So, one solution would be to split the file into separate documents,
+# so that you can process the resulting files as valid XML documents.
+
+import xml.etree.ElementTree as ET
+PATENTS = 'patent.data'
+
+def get_root(fname):
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+
+def split_file(filename):
+    """
+    Split the input file into separate files, each containing a single patent.
+    As a hint - each patent declaration starts with the same line that was
+    causing the error found in the previous exercises.
+
+    The new files should be saved with filename in the following format:
+    "{}-{}".format(filename, n) where n is a counter, starting from 0.
+    """
+    n = 0
+    out_fh = None
+    with open(filename) as in_fh:
+        while True:
+            line = in_fh.readline()
+            if not line: break
+
+            if line.startswith('<?xml'):
+                if out_fh:
+                    out_fh.close()
+                out_fh = open( "{}-{}".format(filename, n), 'wb')
+                out_fh.write(line)
+                n = n + 1
+            out_fh.close()
+
+
+def test():
+    split_file(PATENTS)
+    for n in range(4):
+        try:
+            fname = "{}-{}".format(PATENTS, n)
+            f = open(fname, "r")
+            if not f.readline().startswith("<?xml"):
+                print "You have not split the file {} in the correct boundary!".format(fname)
+            f.close()
+        except:
+            print "Could not find file {}. Check if the filename is correct!".format(fname)
+
+
+test()
